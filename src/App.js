e2a0be9c642cb13be92b820086b157a1989aca2c
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2020-03-30 10:51:00 +0100

    layout: Implemented Footer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Layout } from 'antd';
 import Navbar from './components/Navbar';
 
+const { Footer } = Layout;
+
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Layout>
-            <Navbar />
-          </Layout>
+          {/* <Layout> */}
+          <Navbar />
+
+          <Switch>
+            <Route exact path='/' component={() => <div>hi</div>} />
+          </Switch>
+
+          <Footer style={{ textAlign: 'center' }}>AlgoLearn ©{(new Date().getFullYear())} Created by Oreoluwa Bimbo-Salami</Footer>
+          {/* </Layout> */}
         </div>
       </BrowserRouter>
     </div>
--- src/components/Navbar/SignedIn/index.js
+++ src/components/Navbar/SignedIn/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Inlinks from './Links';
 import InDrawer from './Drawer';
 
