ac82071c529240502a4f7f8c85f1a5a682a5dac5
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2020-04-07 22:56:30 +0100

    File restructuring

--- src/store/context/index.js
+++ src/store/context/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
+import { ConfigProvider } from 'antd';
 import AuthContextProvider from './auth';
 import CourseContextProvider from './course';
 import LessonContextProvider from './lesson';
-import { ConfigProvider } from 'antd';
 // import ColorContextProvider from './colors';
-// import { config } from '../../config';
 // import AdminUserContextProvider from './admin';
 
 function RootContext(props) {
-    // const apiUrl = config.apiUrl;
-    // const apiUrl = 'http://localhost:5000/api/v1'
     return (
         <AuthContextProvider>
             {/* <AdminUserContextProvider >*/}
