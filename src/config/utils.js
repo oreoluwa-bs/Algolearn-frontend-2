b2726ae3f3c1ac1798d6784cdfcdc8ee83304d08
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2020-08-21 19:42:09 +0100

    fixed url

--- src/config/utils.js
+++ src/config/utils.js
@@ -6,7 +6,7 @@ utils.getCookie = (name) => {
     if (parts.length === 2) return parts.pop().split(";").shift();
 }
 
-utils.apiHOST = 'http://localhost:5000/';
+utils.apiHOST = 'https://algolearn-api.herokuapp.com/';
 
 utils.getYearRange = () => {
     const currentYear = (new Date()).getFullYear();
