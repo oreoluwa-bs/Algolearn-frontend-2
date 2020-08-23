011a233f7545351cb286dedaec9cc88e6c7f1ef1
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2020-08-23 07:42:35 +0100

    Fixed review bug

--- src/components/Catalogue/ReviewPreview.js
+++ src/components/Catalogue/ReviewPreview.js
@@ -5,14 +5,14 @@ import { utils } from '../../config'
 const { Paragraph, Text, } = Typography;
 
 const ReviewPreview = ({ review }) => {
-    
+
     const reviewColors = ['#02b3e4', '#02ccba', '#ff5483'];
 
     return (
         <div className='review-card' style={{ borderColor: review.rating > 2 ? review.rating > 3 ? reviewColors[1] : reviewColors[0] : reviewColors[2] }}>
             <div>
-                {review.user.photo && <Avatar size={80} style={{ backgroundColor: '#87d068' }} src={`${utils.apiHOST}images/users/${review.user.photo}`} />}
-                {!review.user.photo && <Avatar size={80} style={{ backgroundColor: '#87d068' }}>{review.user.firstname[0]}{review.user.lastname[0]}</Avatar>}
+                {review.user.photo && <Avatar size={80} style={{ backgroundColor: review.user ?? '#87d068' }} src={`${utils.apiHOST}images/users/${review.user.photo}`} />}
+                {!review.user.photo && <Avatar size={80} style={{ backgroundColor: review.user ?? '#87d068' }}>{review.user.firstname[0]}{review.user.lastname[0]}</Avatar>}
             </div>
             <div><Text type='secondary'>{review.user && review.user.firstname} {review.user && review.user.lastname}</Text></div>
             <div style={{ marginTop: 5 }}>
